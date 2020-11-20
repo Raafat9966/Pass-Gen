4df089588572b75fa571916e4774bea664839faa
Raafat Basheer <raafat.basheer@yahoo.com>
2020-11-20 19:43:12 +0100

    creating password

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var init = () => {
-    document.getElementById("button").addEventListener(
-        "click",
-        function (event) {
-            event.preventDefault();
-            document.getElementById("message").value = Math.floor(
-                Math.random() * 100000000000000
-            );
-        },
-        true
-    );
+	document.getElementById("button").addEventListener(
+		"click",
+		function (event) {
+			event.preventDefault();
+			document.getElementById("message").value = Array(10)
+				.fill(
+					"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+				)
+				.map(function (x) {
+					return x[Math.floor(Math.random() * x.length)];
+				})
+				.join("");
+		},
+		true
+	);
 };
